c7a50ceb4f4f7744e30178e2be836e5f2960d504
Sandra Arriaga <sandraarriaga@Sandys-MacBook-Air.local>
2012-07-21 10:47:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apptastic::Application.config.secret_token = '17a701d87e3f044e3d1e43c000a178ad5aaddfdcc7b7441f3d4a8869e314f8e6c7c9e234f5ed8be9006697e501b193658f75979abb30eb86aa06e256b109dc16'
